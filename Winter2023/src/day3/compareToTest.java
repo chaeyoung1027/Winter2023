77e1e376666776f6588a05ed1ca5788eb736f869
chaeyoung1027 <s2111@e-mirim.hs.kr>
2022-12-28 10:13:06 +0900

    숫자끼리 비교

---
+++ Winter2023/src/day3/compareToTest.java
@@ -0,0 +1,14 @@
+package day3;
+
+public class compareToTest {
+
+	public static void main(String[] args) {
+		Integer x = 3;
+		Integer y = 4;
+		Double z = 1.0;
+		
+		System.out.println(x.compareTo(y));
+
+	}
+
+}
