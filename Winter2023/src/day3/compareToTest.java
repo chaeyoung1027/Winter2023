d5b2095c4308fc574fa23c2470b1f4556c82b20a
chaeyoung1027 <s2111@e-mirim.hs.kr>
2022-12-28 11:08:39 +0900

    비교함수

--- Winter2023/src/day3/compareToTest.java
+++ Winter2023/src/day3/compareToTest.java
@@ -6,9 +6,23 @@ public class compareToTest {
 		Integer x = 3;
 		Integer y = 4;
 		Double z = 1.0;
+		int x1 = 4;
+		int xy = 5;
+		String str = "abcd";
+	
+		System.out.println(Integer.compare(x, y));
 		
 		System.out.println(x.compareTo(y));
+		System.out.println(x.compareTo(3));
+		System.out.println(x.compareTo(3));
+		System.out.println(x.compareTo(2));
 
+		System.out.println(str.compareTo("abcd"));
+		System.out.println(str.compareTo("ab"));		//4-2->2
+		System.out.println(str.compareTo("a"));	//3
+		System.out.println(str.compareTo("c"));	// -2 A와 C의 아스키코드의 차이 C-A
+		
+		System.out.println(str.compareTo("hello"));
 	}
 
 }
